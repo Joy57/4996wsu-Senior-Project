dd9eebf2d83231173100a5a088989588fc406b8a
Joy57 <abedinjoynal2@gmail.com>
2018-09-04 15:08:18 -0400

    minor update

--- WarmUp-TODO-APP/app.js
+++ WarmUp-TODO-APP/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost/todosdatabase',{ useNewUrlParser: true }, (err, response) => {
+mongoose.connect('mongodb://localhost/todo_database',{ useNewUrlParser: true }, (err, response) => {
     console.log('Database Connected...');
 
     if (err) 
