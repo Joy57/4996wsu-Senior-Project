c99c8768cda5b634a8ad56fabe72f3df9f673afd
Joy57 <abedinjoynal2@gmail.com>
2018-11-09 19:31:15 -0500

    update configfiles

--- configFiles/config_nginx.sh
+++ configFiles/config_nginx.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get update -y
+sudo apt-get install nginx -y
 # git clone https://github.com/Joy57/4996wsu-Senior-Project.git
 # cd 4996wsu-Senior-Project
-cd configFiles
-rm /etc/nginx/nginx.conf
-rm /etc/nginx/sites-enabled/default
-cp nginx.conf /etc/nginx/nginx.conf
-cp default /etc/nginx/sites-enabled/default
+# cd configFiles
+sudo rm /etc/nginx/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo cp default /etc/nginx/sites-enabled/default
 sudo service nginx start
+chmod u+x init.sh
 echo "completed all steps..."
 echo "now you can run init.sh"
--- configFiles/init.sh
+++ configFiles/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
 echo "Verifying docker installation..."
-sudo docker run hello-world
+sudo docker run hello-world 
 
 echo "Pulling the weather image and running it"
-docker run --rm -p 8000:8000 joyabe18/test2:pi1
+sudo docker run --rm -p 8000:8000 joyabe18/test2:pi1
 
